767764a2d81e14ce79d502606fa7903d0679488b
Bryan Dela Cruz <bdshi13@outlook.com>
2017-06-22 18:39:30 +0800

    Fixed loading fragmet

--- app/src/main/java/com/mapua/aquajmt/customerapp/fragments/LoadingFragment.java
+++ app/src/main/java/com/mapua/aquajmt/customerapp/fragments/LoadingFragment.java
@@ -1,11 +1,15 @@
 package com.mapua.aquajmt.customerapp.fragments;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 import com.mapua.aquajmt.customerapp.R;
 
@@ -19,7 +23,13 @@ public class LoadingFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_loading, container, false);
-        getDialog().setCanceledOnTouchOutside(false);
+        Dialog dialog = getDialog();
+        dialog.setCanceledOnTouchOutside(false);
+
+        Window window = dialog.getWindow();
+        if (window != null) {
+            window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
 
         return view;
     }
